88a6723b49afbe2e6dd1cb109b9e8695a27044e4
MrDos180 <ilya180@yandex.ru>
2022-12-15 21:55:46 +0300

    add github test

--- src/test/java/com/example/selenide_task11/GitHubTest.java
+++ src/test/java/com/example/selenide_task11/GitHubTest.java
@@ -1,2 +1,32 @@
-package com.example.selenide_task11;public class GitHubTest {
+package com.example.selenide_task11;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class GitHubTest extends TestBase{
+    @Test
+    void gitHubTest(){
+
+        step("Открывыаем страницу", () ->{
+            open("https://github.com/");});
+        step("Ищем репощиторий selenide", () ->{
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys("selenide");
+            $(".header-search-input").submit();});
+        step("Нажать на ссылку репозитория", () ->{
+            $(linkText("selenide/selenide")).click();});
+        step("Открыть таб wiki", () ->{
+            $("#wiki-tab").shouldBe(Condition.visible).click();});
+        step("Ищем page и кликаем по нему", () ->{
+            $(".wiki-more-pages-link").lastChild().click();
+            $x("//div[@id='wiki-pages-box']//a[text()='SoftAssertions']").shouldBe(Condition.visible).click();});
+        step("Проверяем что на странице есть текст", () ->{
+            $("#user-content-3-using-junit5-extend-test-class").shouldBe(Condition.visible);
+        });
+
+    }
 }
--- src/test/java/com/example/selenide_task11/TestBase.java
+++ src/test/java/com/example/selenide_task11/TestBase.java
@@ -1,4 +1,12 @@
 package com.example.selenide_task11;
 
-public class TestData {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.startMaximized = true;
+    }
 }
